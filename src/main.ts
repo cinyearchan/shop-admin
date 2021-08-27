165af0531d4b91b2eadc7bea861618ef79f162be
cinyearchan <cinyearchan@gmail.com>
2021-08-27 10:49:11 +0800

    feat: add css preprocessorOptions

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import { store, key } from './store'
 
+import '@/styles/index.scss'
+
 console.log(import.meta.env.BASE_URL)
 
 createApp(App)
