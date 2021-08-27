90e058a6426ef04cd8ffacf31937dfe4ee7a15eb
cinyearchan <cinyearchan@gmail.com>
2021-08-27 14:43:31 +0800

    chore: element-plus引入及配置

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { store, key } from './store'
+import ElementPlus from '@/plugins/element-plus'
 
 import '@/styles/index.scss'
 
@@ -10,4 +11,5 @@ console.log(import.meta.env.BASE_URL)
 createApp(App)
   .use(router)
   .use(store, key)
+  .use(ElementPlus)
   .mount('#app')
