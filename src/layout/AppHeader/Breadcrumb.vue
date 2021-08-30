1b3fedd222356576a48181c7b6478074e5ef090a
cinyearchan <cinyearchan@gmail.com>
2021-08-30 23:53:43 +0800

    feat: 面包屑导航

---
+++ src/layout/AppHeader/Breadcrumb.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-breadcrumb separetor-class="el-icon-arrow-right">
+    <el-breadcrumb-item
+      v-for="item in routes"
+      :key="item.path"
+    >
+      {{ item.meta.title }}
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+import { computed } from 'vue'
+
+const router = useRouter()
+
+const routes = computed(() => {
+  return router.currentRoute.value.matched.filter(item => item.meta.title)
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -13,7 +13,10 @@ const routes: RouteRecordRaw[] = [
       {
         path: '',
         name: 'home',
-        component: () => import('../views/home/index.vue')
+        component: () => import('../views/home/index.vue'),
+        meta: {
+          title: '首页'
+        }
       },
       product,
       media,
