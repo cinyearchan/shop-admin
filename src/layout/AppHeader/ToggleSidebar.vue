5a5bede4a143baf9521f86b33b935ff3934c40dd
cinyearchan <cinyearchan@gmail.com>
2021-08-30 17:10:56 +0800

    feat: 侧边栏展开收起

---
+++ src/layout/AppHeader/ToggleSidebar.vue
@@ -0,0 +1,12 @@
+<template>
+  <i
+    :class="$store.state.isCollapse ? 'el-icon-s-unfold' : 'el-icon-s-fold'"
+    @click="$store.commit('setIsCollapse', !$store.state.isCollapse)"
+  />
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/layout/AppHeader/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <ToggleSidebar />
+  <Breadcrumb />
+  <MenuSearch />
+  <FullScreen />
+  <Notification />
+  <UserInfo />
+</template>
+
+<script lang="ts" setup>
+import Breadcrumb from './Breadcrumb.vue'
+import FullScreen from './FullScreen.vue'
+import MenuSearch from './MenuSearch.vue'
+import Notification from './Notification.vue'
+import ToggleSidebar from './ToggleSidebar.vue'
+import UserInfo from './UserInfo.vue'
+
+</script>
