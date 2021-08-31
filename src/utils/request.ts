63de047f20d36c9e2833d9eddc4ee98d3469bb36
cinyearchan <cinyearchan@gmail.com>
2021-08-31 17:13:25 +0800

    feat: 统一处理登录过期

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,10 +1,13 @@
 import axios, { AxiosRequestConfig } from 'axios'
-import { ElMessage } from 'element-plus'
+import { ElMessage, ElMessageBox } from 'element-plus'
 import { store } from '@/store'
+import router from '@/router/'
 
 const request = axios.create({
   baseURL: import.meta.env.VITE_API_BASEURL
 })
+
+let isRefreshing = false
 
 request.interceptors.request.use(config => {
   const user = store.state.user
@@ -19,11 +22,35 @@ request.interceptors.request.use(config => {
 })
 
 request.interceptors.response.use(response => {
-  if (response.data.status && response.data.status !== 200) {
-    ElMessage.error(response.data.msg || '请求失败，请稍后重试')
-    return Promise.reject(response.data)
+  const status = response.data.status
+  // 正常情况
+  if (!status || status === 200) {
+    return response
   }
-  return response
+  // token 失效
+  if (status === 410000) {
+    if (isRefreshing) return Promise.reject(response)
+    isRefreshing = true
+    ElMessageBox.confirm('您的登录已过期，您可以取消停留在此页面，或确认重新登录', '登录过期', {
+      confirmButtonText: '确认',
+      cancelButtonText: '取消'
+    }).then(() => {
+      store.commit('setUser', null)
+
+      router.push({
+        name: 'login',
+        query: {
+          redirect: router.currentRoute.value.fullPath
+        }
+      })
+    }).finally(() => {
+      isRefreshing = false
+    })
+    return Promise.reject(response)
+  }
+  // 其他错误
+  ElMessage.error(response.data.msg || '请求失败，请稍后重试')
+  return Promise.reject(response)
 }, error => {
   return Promise.reject(error)
 })
